409ac6417681404f38706f95d7e4f5d064885f8f
Diaz Amaliana <diazamaliana@gmail.com>
2021-07-13 22:03:33 +0700

    add Playlist components in `App.js`

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Playlist from "./components/Playlist";
+import data from "./data/Playlist";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="" style={{ textAlign: "Center" }}>Create Playlist</h1>
+      <div className="song-wrapper">
+          <Playlist data={data}/>
+      </div>
     </div>
   );
 }
