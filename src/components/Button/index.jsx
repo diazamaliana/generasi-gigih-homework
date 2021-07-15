4a8205e0268ce3cecc0fb174f3a12f6f3ac3c205
Diaz Amaliana <diazamaliana@gmail.com>
2021-07-15 16:10:48 +0700

    add Button components

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './style.css';
+
+const STYLES = [
+    "btn--primary--solid",
+    "btn--primary--outline"
+];
+
+const SIZES = [ "btn--medium", "btn--large","btn--small"];
+
+const Button = ({
+    children, 
+    type, 
+    onClick, 
+    buttonStyle, 
+    buttonSize
+}) => {
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle: STYLES[0];
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize: SIZES[0];
+
+    return (
+        <button 
+            className={`btn ${checkButtonStyle} ${checkButtonSize}`} 
+            onClick={onClick} 
+            type={type}
+        >
+            {children}
+        </button>
+    )
+};
+
+export default Button;
