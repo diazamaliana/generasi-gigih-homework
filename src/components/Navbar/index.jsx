f6ce065c046f918a6f86cde4fb0e784676e3d849
Diaz Amaliana <diazamaliana@gmail.com>
2021-07-21 14:35:19 +0700

    add Navbar Component

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,35 @@
+import Button from "../Button";
+import SearchBar from "../SearchBar";
+import Profile from "../Profile";
+import { redirect } from "../../config/Login";
+import Style from "./style.module.css";
+
+
+
+
+const Navbar = ({ userData, handleSearch }) => {
+    const isLogin = userData?.access_token !== undefined;
+    return (
+      <div className={Style.Navbar}>
+        {isLogin ?
+          (
+            <>            
+              <SearchBar handleSearch={handleSearch} />
+              <Profile userData={userData} />
+            </>
+          ) : (
+           <>
+            
+            <Button 
+              type="button"
+              buttonStyle="btn--primary--solid"
+              buttonSize="btn--medium" 
+              onClick={() => redirect()}
+              >Login</Button> 
+          </>)
+        }        
+      </div>
+    );
+  };
+  
+  export default Navbar;
