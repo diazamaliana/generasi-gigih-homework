d1c0a974c1df73ca5b6469a96698993f3a3606aa
Diaz Amaliana <diazamaliana@gmail.com>
2021-08-03 02:04:51 +0700

    add private route

---
+++ src/components/PrivateRoute/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const PrivateRoute = ({component: Component, ...rest}) => {
+
+    const { isAuthenticated } = useSelector(state => state.userAuth)
+
+    return (
+        // Show the component only when the user is logged in
+        // Otherwise, redirect the user to / page
+        <Route {...rest} render={props => (
+            isAuthenticated ?
+                <Component {...props} />
+            : <Redirect to="/" />
+        )} />
+    );
+};
+
+export default PrivateRoute;
