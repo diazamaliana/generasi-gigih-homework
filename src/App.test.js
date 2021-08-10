89f8987db9ed0cb706d962918f27a88f19014e7d
Diaz Amaliana <diazamaliana@gmail.com>
2021-08-11 00:55:14 +0700

    write a test for login page

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import App from './App';
+import { store } from "./redux";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+global.matchMedia =
+  global.matchMedia ||
+  function () {
+    return {
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+    };
+  };
+
+test('renders login page', () => {
+  render(
+    <Provider store={store}>
+       <App />
+    </Provider>
+  );
+  const title = screen.getByText(/Listening is everything/i);
+  expect(title).toBeVisible();
+
+  const subtitle = screen.getByText(/Millions of songs and podcasts/i)
+  expect (subtitle).toBeVisible();
+
+  const loginButton = screen.getByText(/log in/i)
+  expect(loginButton).toBeInTheDocument();
 });
