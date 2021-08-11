f1f74830be4b07faf8c670e9eb40057c78d2b05b
Diaz Amaliana <diazamaliana@gmail.com>
2021-08-11 09:28:14 +0700

    write a test for track component

---
+++ src/components/TrackList/TrackList.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react";
+import TrackList from "./index";
+import userEvent from "@testing-library/user-event";
+import { Provider } from 'react-redux';
+import { store } from "../../redux";
+import data from '../../data/SingleTrack'
+
+
+global.matchMedia =
+  global.matchMedia ||
+  function () {
+    return {
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+    };
+}
+
+test("tracks component are rendered correctly", () => {
+    render(
+    <Provider store={store}>
+        <TrackList  track={data} />
+    </Provider>
+    );
+
+    const albumName = screen.getByTestId("album-name");
+    const trackName = screen.getByTestId("track-name");
+    const selectTrackButton = screen.getByText(/select/i);
+    expect(albumName).toBeVisible();
+    expect(albumName).toHaveTextContent(data.album.name);
+    expect(trackName).toBeVisible();
+    expect(trackName).toHaveTextContent(data.name);
+    expect(selectTrackButton).toBeVisible();
+    // screen.debug();
+  });
+
